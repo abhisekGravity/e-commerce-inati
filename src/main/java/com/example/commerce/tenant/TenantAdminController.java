107bd556971828637252ee990315cc76b8e7f385
abhisekGravity <anayak@gravityer.com>
2026-01-24 13:30:36 +0530

    ..

--- src/main/java/com/example/commerce/tenant/TenantAdminController.java
+++ src/main/java/com/example/commerce/tenant/TenantAdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tenants/admin")
+@RequestMapping("/tenants")
 @RequiredArgsConstructor
 public class TenantAdminController {
 
